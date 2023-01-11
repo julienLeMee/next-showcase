9aed33b9917be943196666766926fd586ed16fdd
Julien Le Mee <julemee@gmail.com>
2023-01-11 17:48:53 -0500

    added about components

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,7 +11,7 @@ export default function Main() {
           <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHIMG TOGETHER</p>
           <h1 className='py-4 text-gray-700'>Hi I'm <span className='text-[#5651e5]'>Julien</span></h1>
           <h1 className='py-2 text-gray-700'>A Web Developer</h1>
-          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm based in Montréal, Canada. Currently, I'm focused on building digital experiences with three.js and responsive and fast website with Next.js.</p>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm based in Montréal, Canada. Currently, I'm focused on building digital experiences with <span className='text-[#5651e5]'>three.js</span> and responsive and fast website with <span className='text-[#5651e5]'>Next.js</span>.</p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
               <FaLinkedinIn />
