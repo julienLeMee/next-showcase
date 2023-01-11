e73c099695b6fc7adc75cf04084d06f7d1d5735a
Julien Le Mee <julemee@gmail.com>
2023-01-11 17:12:02 -0500

    added main component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 export default function Main() {
   return (
-    <h1>Title</h1>
+    <div className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+          <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHIMG TOGETHER</p>
+          <h1 className='py-4 text-gray-700'>Hi I'm <span className='text-[#5651e5]'>Julien</span></h1>
+          <h1 className='py-2 text-gray-700'>A Web Developer</h1>
+          <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'm based in Montréal, Canada. Currently, I'm focused on building digital experiences with three.js and responsive and fast website with Next.js.</p>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaLinkedinIn />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaGithub />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineMail />
+            </div>
+            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
